06d160ac75ebdf1cb9776740bdea112bb845354a
Mohammed Mamun <momamun@yahoo.com>
2020-07-27 14:17:44 -0400

    Mapping of DTO and Entity

--- src/main/java/com/reddit/clone/service/SubredditService.java
+++ src/main/java/com/reddit/clone/service/SubredditService.java
@@ -23,19 +23,19 @@ public class SubredditService {
     private final SubredditRepository subredditRepository;
     private final SubredditMapper subredditMapper;
 
+    @Transactional
+    public SubredditDto save(SubredditDto subredditDto) {
+        Subreddit subreddit = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
+        subredditDto.setId(subreddit.getId());
+        return subredditDto;
+    }
+    
     @Transactional(readOnly = true)
     public List<SubredditDto> getAll() {
         return subredditRepository.findAll()
                 .stream()
                 .map(subredditMapper::mapSubredditToDto)
                 .collect(toList());
-    }
-
-    @Transactional
-    public SubredditDto save(SubredditDto subredditDto) {
-        Subreddit subreddit = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
-        subredditDto.setId(subreddit.getId());
-        return subredditDto;
     }
 
 
