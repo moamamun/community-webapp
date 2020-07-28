0f511fdae93b0600124901361b9c1d0758eeed34
Mohammed Mamun <momamun@yahoo.com>
2020-07-28 14:38:32 -0400

    Impl Get Mapping

--- src/main/java/com/reddit/clone/controller/CommentController.java
+++ src/main/java/com/reddit/clone/controller/CommentController.java
@@ -1,18 +1,39 @@
 package com.reddit.clone.controller;
 
 import com.reddit.clone.dto.CommentsDto;
+import com.reddit.clone.service.CommentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/comments/")
 @AllArgsConstructor
 public class CommentController {
 
+    private final CommentService commentService;
+
     @PostMapping
-    public void createComment(@RequestBody CommentsDto commentsDto) {
+
+    //convert object into our comment entity and save it to the database (handled in service class)
+    public ResponseEntity<Object> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @GetMapping("/by-post/{postId}")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@PathVariable Long postId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(commentService.getAllCommentsForPost(postId));
+    }
+
+    @GetMapping("/by-user/{userName}")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@PathVariable String userName){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(commentService.getAllCommentsForUser(userName));
+    }
+
 }
