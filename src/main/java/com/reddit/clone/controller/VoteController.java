41220e131ebc3cf72e49c53b63688bd15544bcdd
Mohammed Mamun <momamun@yahoo.com>
2020-07-29 11:26:51 -0400

    VoteController init

---
+++ src/main/java/com/reddit/clone/controller/VoteController.java
@@ -0,0 +1,12 @@
+package com.reddit.clone.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/votes/")
+@AllArgsConstructor
+public class VoteController {
+
+}
