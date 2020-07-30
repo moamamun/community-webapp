c42ab0cd49128f989c037f072c39da4e95444128
Mohammed Mamun <momamun@yahoo.com>
2020-07-30 14:43:08 -0400

    Added PostMapping for Refresh token and logout

--- src/main/java/com/reddit/clone/controller/AuthController.java
+++ src/main/java/com/reddit/clone/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.reddit.clone.controller;
 
 import com.reddit.clone.dto.AuthenticationResponse;
 import com.reddit.clone.dto.LoginRequest;
+import com.reddit.clone.dto.RefreshTokenRequest;
 import com.reddit.clone.dto.RegisterRequest;
 import com.reddit.clone.service.AuthService;
+import com.reddit.clone.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
@@ -32,5 +37,17 @@ public class AuthController {
     @PostMapping("/login")
     public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
+    }
+
+    @PostMapping("refresh/token")
+    //taking request body of type rt request
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(HttpStatus.OK).body("Refresh Token Deleted Successfully!!");
     }
 }
