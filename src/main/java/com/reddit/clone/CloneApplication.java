ba49398602de0822d7892b54d9f2f56b7b0ae448
Mohammed Mamun <momamun@yahoo.com>
2020-08-05 20:40:15 -0400

    Import Swagger

--- src/main/java/com/reddit/clone/CloneApplication.java
+++ src/main/java/com/reddit/clone/CloneApplication.java
@@ -1,11 +1,17 @@
 package com.reddit.clone;
 
+import com.reddit.clone.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+// With import at application startup spring will invoke spring fox -->
+// will scan all the rest controllers components in our app and the corresponding dtos for this rest controllers
+//--> generate the rest API documentation for all of these
+@Import(SwaggerConfig.class)
 public class CloneApplication {
 
 	public static void main(String[] args) {
